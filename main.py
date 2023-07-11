5a378c09945d9d922251ce0cea3ef9c9f964d928
Julia-fsiqueira <cfsiqueira.julia@gmail.com>
2023-07-11 09:15:59 -0300

    refactor/refaz o codigo

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ while True:
             }
             funções.titulo(novo_livro)
         elif opcao in 'P':
-            funções.pesquisa_indice(file)
+            funções.pesquisa_indice()
         elif opcao in 'A':
-            funções.atualiza_titulo(file)
+            funções.atualiza_titulo()
         elif opcao in 'D':
-            funções.deleta_titulo(file)
+            funções.deleta_titulo()
         elif opcao in 'S':
             print(f'{"FIM. VOLTE SEMPRE":^40}')
             break
